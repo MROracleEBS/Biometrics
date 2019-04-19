e37e2fc24d19860ac2195180ee49512218611649
Mohamed Ramadan Mohamed Hassan <m_ramadan2012@hotmail.com>
2019-04-19 15:41:01 +0300

    Update my tanserflow test file

--- TanserFlow.py
+++ TanserFlow.py
@@ -1,7 +1,21 @@
 import tensorflow as tf
 
 print(tf.VERSION)
-
+###########################################
+a = tf.add(3, 5)
+sess = tf.Session()
+print (sess.run(a))
+sess.close()
+###########################################
+xx = 2
+yy = 3
+op1 = tf.add(xx, yy)
+op2 = tf.mul(xx, yy)
+op3 = tf.pow(op2, op1)
+with tf.Session() as sess:
+    op3 = sess.run(op3)
+sess.close()
+###########################################
 x = 2
 y = 3
 add_op =  tf.add(x, y)
@@ -10,6 +24,6 @@ useless =  tf.multiply(x, add_op)
 pow_op =  tf.pow(add_op, mul_op)
 with tf.Session() as sess:
     z = sess.run(pow_op)
-
 print(z)
 sess.close()
+###########################################
